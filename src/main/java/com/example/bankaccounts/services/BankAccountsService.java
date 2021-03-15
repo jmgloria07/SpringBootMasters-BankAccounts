2ccdc59f2b6e4993c29631caee21b32ccd721e91
Eric Anastacio <erican@magenic.com>
2021-03-15 10:54:25 +0800

    format

--- src/main/java/com/example/bankaccounts/services/BankAccountsService.java
+++ src/main/java/com/example/bankaccounts/services/BankAccountsService.java
@@ -7,8 +7,12 @@ import com.example.bankaccounts.model.TransactionsDTO;
 
 public interface BankAccountsService {
 	public Account get(long id);
+
 	public List<Account> get();
+
 	public Account createUpdate(Account account);
+
 	public void deleteById(long id);
+
 	public Account createTransaction(long id, TransactionsDTO transactionsDTO);
 }
